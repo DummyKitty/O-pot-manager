ec6f817728249ba959d25c653f55608047904fc7
Dr34d <40905775+GreyDr34d@users.noreply.github.com>
2021-04-16 11:52:11 +0800

    init

---
+++ .history/main_20210416112645.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@文件        :main.py
+@说明        :
+@时间        :2021/04/16 11:23:08
+@作者        :dr34d
+@版本        :1.0
+'''
+
+def main():
+    
+
+if __name__ == '__main__':
+    main()
---
+++ .history/manager/main_20210416113303.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File        :main.py
+@Description :
+@Date        :2021/04/16 11:28:01
+@Author      :dr34d
+@Version     :1.0
+'''
+
+from tools_console.
+
+def check_env():
+    pass
+
+def init_option():
+    pass
+
+
+
+def main():
+    check_env()
+    init_option()
+    s = 
+    start()
+
+if __name__ == '__main__':
+    main()
