f3094b324ca105c758aca7a6c7f3756ca74fc7a4
Dr34d <40905775+GreyDr34d@users.noreply.github.com>
2021-04-25 16:19:46 +0800

    remove

--- manager/lib/core/data.py
+++ manager/lib/core/data.py
@@ -30,10 +30,10 @@ DATA_DIR = BASE_DIR + '/data/'
 TEMPLATE_DIR = BASE_DIR + '/template/'
 DATABASE_PATH = DATA_DIR + 'data.db'
 CURRENT_REVERSE_PROXIES_PATH = DATA_DIR + 'current_reverse_proxy.txt'
-CENSYS_UID = "b7baa610-b4c6-463e-af4b-dea66412ea0a"
-CENSYS_SECRET = "hkOJASynWbs7obh26grY7h2xIWS4Ne9T"
-ZOOMEYE_USERNAME = "3214436480@qq.com"
-ZOOMEYE_PASSWORD = "aa15074520721"
+CENSYS_UID = ""
+CENSYS_SECRET = ""
+ZOOMEYE_USERNAME = ""
+ZOOMEYE_PASSWORD = ""
 
 if __name__ == "__main__":
     print(BANNER)
